bfc375f6864ecafd033ee4b70a93c503824d9116
Baptiste M <baptiste.montagliani@epitech.eu>
2019-05-29 19:47:43 -0700

    M38U8

---
+++ M3U8Kit-tvOS/M3U8Kit_tvOS.h
@@ -0,0 +1,19 @@
+//
+//  M3U8Kit_tvOS.h
+//  M3U8Kit-tvOS
+//
+//  Created by Baptiste on 29/05/2019.
+//  Copyright © 2019 Allen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for M3U8Kit_tvOS.
+FOUNDATION_EXPORT double M3U8Kit_tvOSVersionNumber;
+
+//! Project version string for M3U8Kit_tvOS.
+FOUNDATION_EXPORT const unsigned char M3U8Kit_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <M3U8Kit_tvOS/PublicHeader.h>
+
+
---
+++ M3U8KitDynamic-tvOS/M3U8KitDynamic_tvOS.h
@@ -0,0 +1,19 @@
+//
+//  M3U8KitDynamic_tvOS.h
+//  M3U8KitDynamic-tvOS
+//
+//  Created by Baptiste on 29/05/2019.
+//  Copyright © 2019 Allen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for M3U8KitDynamic_tvOS.
+FOUNDATION_EXPORT double M3U8KitDynamic_tvOSVersionNumber;
+
+//! Project version string for M3U8KitDynamic_tvOS.
+FOUNDATION_EXPORT const unsigned char M3U8KitDynamic_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <M3U8KitDynamic_tvOS/PublicHeader.h>
+
+
